a919b99c2b0bacdc7a50202518a3ac9e419f9f88
jtijero <jtijero@unsa.edu.pe>
2025-05-11 21:38:13 -0500

    se agregan las 3 primeras funciones que probaremos proximamente con square

---
+++ PW2_LAB5_Python_Chess/src/picture.py
@@ -0,0 +1,12 @@
+from colors import *
+class Picture:
+  def __init__(self, img):
+    self.img = img;
+
+  def __eq__(self, other):
+    return self.img == other.img
+
+  def _invColor(self, color):
+    if color not in inverter:
+      return color
+    return inverter[color]
