810e092ba8525752f951c90a3d3bd19b73f1e282
jtijero <jtijero@unsa.edu.pe>
2025-05-12 12:50:09 -0500

    probé verticalMirror y tambien horizontalMirror, para crear la figura de los 4 caballos de colores intercalados y mirando para el lado contrario en la fila 2

--- PW2_LAB5_Python_Chess/src/chess.py
+++ PW2_LAB5_Python_Chess/src/chess.py
@@ -7,11 +7,11 @@ white_knight = Picture(KNIGHT)  # Caballo blanco
 black_knight = Picture(KNIGHT).negative()  # Caballo negro (negativo del blanco)
 
 # Crear las filas de caballos
-first_row = white_knight.horizontalRepeat(2)  # 2 caballos blancos en la primera fila
-second_row = black_knight.horizontalRepeat(2)  # 2 caballos negros en la segunda fila
+first_row = white_knight.horizontalRepeat(1).join(black_knight.horizontalRepeat(1)) 
+second_row = first_row.verticalMirror() 
 
 # Combinar las filas
-combined_picture = first_row.up(second_row)  # Colocar la fila de caballos negros sobre la de caballos blancos
+fila1y2 = first_row.up(second_row)  # Colocar la fila de caballos negros sobre la de caballos blancos
 
 # Dibujar la imagen combinada
-draw(combined_picture)
+draw(fila1y2)
