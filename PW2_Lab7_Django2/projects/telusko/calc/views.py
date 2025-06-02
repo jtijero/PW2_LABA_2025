ba4c78dae69807beec089baddb5c30a3fe5ba8c5
jtijero <jtijero@unsa.edu.pe>
2025-06-02 13:02:46 -0500

    addition of two Numbers in Django: se agrega int para obtener verdadero resultado de la suma

--- PW2_Lab7_Django2/projects/telusko/calc/views.py
+++ PW2_Lab7_Django2/projects/telusko/calc/views.py
@@ -6,8 +6,8 @@ def home(request):
   return render(request,'home.html',{'name':'Lucciano'})
 def add(request):
 
-  val1=request.GET['num1']
-  val2=request.GET['num2']
+  val1=int(request.GET['num1'])
+  val2=int(request.GET['num2'])
   res = val1 + val2
 
   return render(request,"result.html", {'result':res})
