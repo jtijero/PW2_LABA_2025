9da43fdb95b17825280757eb1417b25f2725e015
jtijero <jtijero@unsa.edu.pe>
2025-06-02 23:10:53 -0500

    Passing Dynamic Data in HTML Part-2: modificacion de atributos de destinations

--- PW2_Lab7_Django2/projects/telusko/travello/views.py
+++ PW2_Lab7_Django2/projects/telusko/travello/views.py
@@ -6,5 +6,27 @@ def index(request):
   dest1 = Destination()
   dest1.name = "Mumbai"
   dest1.desc = 'The City that Never Sleeps'
+  dest1.img = 'destination_1.jpg'
   dest1.price = 700
-  return render(request,"index.html",{'dest1': dest1})
+
+  dest2 = Destination()
+  dest2.name = "Hyderabad"
+  dest2.desc = 'First Bityani, Then Sherwani'
+  dest2.img = 'destination_2.jpg'
+  dest2.price = 650
+
+  dest3 = Destination()
+  dest3.name = "Bengaluru"
+  dest3.desc = 'Beautiful City'
+  dest3.img = 'destination_3.jpg'
+  dest3.price = 750
+
+  dest4 = Destination()
+  dest4.name = "Miami Beach"
+  dest4.desc = 'The City that Never Sleeps too'
+  dest4.img = 'destination_4.jpg'
+  dest4.price = 780
+
+  dests =[dest1,dest2,dest3,dest4]
+
+  return render(request,"index.html",{'dests': dests})
