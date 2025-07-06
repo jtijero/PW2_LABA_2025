7714d60fb0e481545d7c24cac99a96a13562e4b0
jtijero <jtijero@unsa.edu.pe>
2025-07-06 17:28:35 -0500

    Angular : app motos > Servicios - mostrar productos: Inyeccion de dependencias para Productos

--- PW2_Lab9_10_Angular/motos/src/app/pages/products/products.ts
+++ PW2_Lab9_10_Angular/motos/src/app/pages/products/products.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Product } from '../../services/products';
+import { Products } from '../../services/products';
 @Component({
   selector: 'app-products',
   imports: [],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
-export class Products {
-
+export class Productos {
+  productos : Product[] = [];
+  constructor(private product:Products){ //esto es export class Products { de services
+    this.productos = this.product.getproducts();
+  }
 }
